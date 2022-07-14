fcd3424bd62cd06316c0cd42c8aed7886097f0d5
cliff <gitacliff48@gmail.com>
2022-07-14 12:06:46 +0300

    appoit1:configure Appointment Follow-up Task

--- config/tasks.js
+++ config/tasks.js
@@ -106,29 +106,29 @@ module.exports = [
     },
 
   },
-  // {
-  //   name: 'lab-test-task',
-  //   title: 'Lab Test Result task',
-  //   icon: 'assessment',
-  //   appliesTo: 'reports',
-  //   appliesToType: ['appointment'],
-  //   appliesIf: function(c, r){
-  //     return r.fields.appoint.type_appoint === 'lab_test';
-  //   },
-  //   actions: [{ form: 'lab', }],
-  //   events: [{
-  //     start: 1,
-  //     days: 1,
-  //     end: 1,
-  //   }],
-  //   resolvedIf: function(c, r, event, dueDate) {
-  //     // Resolved if there is lab received in time window
-  //     return isFormFromArraySubmittedInWindow(c.reports, 'lab',
-  //                Utils.addDate(dueDate, -event.start).getTime(),
-  //                Utils.addDate(dueDate,  event.end+1).getTime());
-  //   },
+  {
+    name: 'appointment-follow-up-task',
+    title: 'Appointment Follow-up Task',
+    icon: 'assessment',
+    appliesTo: 'reports',
+    appliesToType: ['appointment'],
+    appliesIf: function(c, r){
+      return r.fields.appoint.type_appoint === 'clinical' || r.fields.appoint.type_appoint === 'social' || r.fields.appoint.type_appoint === 'case';
+    },
+    actions: [{ form: 'viral', }],
+    events: [{
+      start: 2,
+      days: 1,
+      end: 1,
+    }],
+    resolvedIf: function(c, r, event, dueDate) {
+      // Resolved if there is lab received in time window
+      return isFormFromArraySubmittedInWindow(c.reports, 'viral',
+                 Utils.addDate(dueDate, -event.start).getTime(),
+                 Utils.addDate(dueDate,  event.end+1).getTime());
+    },
 
-  // },
+  },
   {
     name: 'care-assessment-task',
     title: 'Level of care assessment task',
